7b6360ae67378faaf204d6f14184a26eb95cec03
zabilsabri <zabilsabrimuhammad0@gmail.com>
2025-05-24 11:16:11 +0800

    `feat`: add sidebar partial with styling

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
+<div id="mySidebar" class="sidebar bg-white p-2 rounded font-small">
+    <div class="sidebar-header rounded-4 mb-3">
+        <p class="p-0 m-0">Hello,</p>
+        <p class="p-0 m-0 fw-bolder">Halo, Zabil</p>
+    </div>
+    <a href="#" class="active my-1"><span class="ps-3 font-small"><i class="fi fi-rs-house-chimney me-2"></i>Beranda</span></a>
+    <a href="#" class="my-1"><span class="ps-3 font-small"><i class="fi fi-rs-search me-2"></i>Cari</span></a>
+    <a href="#" class="my-1"><span class="ps-3 font-small"><i class="fi fi-rs-document me-2"></i>Skripsi</span></a>
+    <hr>
+    <a href="#" class="my-1"><span class="ps-3 font-small"><i class="fi fi-rs-user me-2"></i>Akun</span></a>
+    <a href="#" class="my-1"><span class="ps-3 font-small"><i class="fi fi-rs-book me-2"></i>Tutorial</span></a>
+</div>

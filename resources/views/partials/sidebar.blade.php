f1765783e020a1dbfdd07da26a09ab05ff98e8ff
zabilsabri <zabilsabrimuhammad0@gmail.com>
2025-05-24 20:23:06 +0800

    `fix`: correct greeting text in sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,8 +1,8 @@
 <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
 <div id="mySidebar" class="sidebar bg-white p-2 rounded font-small">
     <div class="sidebar-header rounded-4 mb-3">
-        <p class="p-0 m-0">Hello,</p>
-        <p class="p-0 m-0 fw-bolder">Halo, Zabil</p>
+        <p class="p-0 m-0">Halo,</p>
+        <p class="p-0 m-0 fw-bolder">Selamat Datang</p>
     </div>
     <a href="#" class="active my-1"><span class="ps-3 font-small"><i class="fi fi-rs-house-chimney me-2"></i>Beranda</span></a>
     <a href="#" class="my-1"><span class="ps-3 font-small"><i class="fi fi-rs-search me-2"></i>Cari</span></a>

6f286d6569370a0b48a36be3127140e3611e3348
zabilsabri <zabilsabrimuhammad0@gmail.com>
2025-05-26 02:00:36 +0800

    `feat`: add routes for login, profile, and logout functionality

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Home\HomeController;
 use App\Http\Controllers\Thesis\ThesisController;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Profile\ProfileController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/thesis', [ThesisController::class, 'index'])->name('thesis');
+Route::get('/login', [AuthController::class, 'index'])->name('login');
+Route::get('/profile', [ProfileController::class, 'index'])->middleware('auth')->name('profile');
+Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
+
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+Route::post('/profile/update', [ProfileController::class, 'updateProfile'])->middleware('auth')->name('profile.update');
+Route::post('/upload-profile', [ProfileController::class, 'upload'])->middleware('auth')->name('profile.upload');

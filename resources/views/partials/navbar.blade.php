8c2d6059f512018406326fec165a0e8bf7fcdc6e
zabilsabri <zabilsabrimuhammad0@gmail.com>
2025-05-26 02:00:06 +0800

    `fix`: correct navbar brand link to route home

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,24 @@
 <nav class="navbar border-bottom bg-white">
   <div class="container" style="padding-left: 15px !important;">
-    <a class="navbar-brand" style="margin-right: 0px" href="#">
+    <a class="navbar-brand" style="margin-right: 0px" href="{{ route('home') }}">
         <img src="{{ asset('image/silog-logo.png') }}" style="object-fit: cover; overflow: hidden;" alt="Bootstrap" class="img-navbar" width="105" height="40">
     </a>
+    <div class="dropdown" style="padding-right: 15px !important;">
+        <a href="" data-bs-toggle="dropdown" class="dropdown-toggle link-underline link-underline-opacity-0 border p-2 rounded-pill border-primary" style="color: #003370; !important" aria-expanded="false">
+          @if(Auth::check())
+            <img src="{{ (Auth::user()->profile_picture == null) ? asset('image/profile-picture/profile-picture-default.jpg') : asset('storage/' . Auth::user()->profile_picture) }}" class="object-fit-cover profile-picture" alt="Profile Picture" width="30" height="30">
+          @else
+            <img src="{{ asset('image/profile-picture/profile-picture-default.jpg') }}" class="profile-picture" alt="Profile Picture" width="30" height="30">
+          @endif
+        </a>
+        <ul class="dropdown-menu font-smaller">
+          @if(Auth::check())
+          <li><a class="dropdown-item" href="{{ route('profile') }}">Profile</a></li>
+          <li><a class="dropdown-item text-danger" href="{{ route('logout') }}">Logout</a></li>
+          @else
+          <li><a class="dropdown-item" href="{{ route('login') }}">Login</a></li>
+          @endif
+        </ul>
+      </div>
   </div>
 </nav>

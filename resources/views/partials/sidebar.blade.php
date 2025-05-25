6dda63b40b0e7d3b85985aade9b83e65800ed49e
zabilsabri <zabilsabrimuhammad0@gmail.com>
2025-05-26 01:59:56 +0800

    `fix`: update profile link in sidebar for correct routing

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -7,6 +7,6 @@
     <a href="{{ route('home') }}" class="{{ request()->routeIs('home') ? 'active' : '' }} my-1"><span class="ps-3 font-small"><i class="fi fi-rs-house-chimney me-2"></i>Beranda</span></a>
     <a href="{{ route('thesis') }}" class="{{ request()->routeIs('thesis') ? 'active' : '' }} my-1"><span class="ps-3 font-small"><i class="fi fi-rs-document me-2"></i>Skripsi</span></a>
     <hr>
-    <a href="#" class="my-1"><span class="ps-3 font-small"><i class="fi fi-rs-user me-2"></i>Akun</span></a>
+    <a href="{{ route('profile') }}" class="{{ request()->routeIs('profile') ? 'active' : '' }} my-1"><span class="ps-3 font-small"><i class="fi fi-rs-user me-2"></i>Akun</span></a>
     <a href="#" class="my-1"><span class="ps-3 font-small"><i class="fi fi-rs-book me-2"></i>Tutorial</span></a>
 </div>

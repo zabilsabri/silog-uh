a252a5ef20b00f114ea1fa1009b3f6620317a08f
zabilsabri <zabilsabrimuhammad0@gmail.com>
2025-05-25 12:16:55 +0800

    `feat`: add thesis route to web.php for thesis listing

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Home\HomeController;
+use App\Http\Controllers\Thesis\ThesisController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/thesis', [ThesisController::class, 'index'])->name('thesis');

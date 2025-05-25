25157c10ae06d61388af300aa5555569bccf5e66
zabilsabri <zabilsabrimuhammad0@gmail.com>
2025-05-25 12:15:53 +0800

    `fix`: update sidebar links to use route helpers for navigation

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -4,9 +4,8 @@
         <p class="p-0 m-0">Halo,</p>
         <p class="p-0 m-0 fw-bolder">Selamat Datang</p>
     </div>
-    <a href="#" class="active my-1"><span class="ps-3 font-small"><i class="fi fi-rs-house-chimney me-2"></i>Beranda</span></a>
-    <a href="#" class="my-1"><span class="ps-3 font-small"><i class="fi fi-rs-search me-2"></i>Cari</span></a>
-    <a href="#" class="my-1"><span class="ps-3 font-small"><i class="fi fi-rs-document me-2"></i>Skripsi</span></a>
+    <a href="{{ route('home') }}" class="{{ request()->routeIs('home') ? 'active' : '' }} my-1"><span class="ps-3 font-small"><i class="fi fi-rs-house-chimney me-2"></i>Beranda</span></a>
+    <a href="{{ route('thesis') }}" class="{{ request()->routeIs('thesis') ? 'active' : '' }} my-1"><span class="ps-3 font-small"><i class="fi fi-rs-document me-2"></i>Skripsi</span></a>
     <hr>
     <a href="#" class="my-1"><span class="ps-3 font-small"><i class="fi fi-rs-user me-2"></i>Akun</span></a>
     <a href="#" class="my-1"><span class="ps-3 font-small"><i class="fi fi-rs-book me-2"></i>Tutorial</span></a>

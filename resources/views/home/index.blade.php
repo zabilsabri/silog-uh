ecd4d03a9fd0317472654210d37437d8a700c2fb
zabilsabri <zabilsabrimuhammad0@gmail.com>
2025-05-25 12:15:06 +0800

    `refactor`: remove unused archive section from home page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -17,55 +17,5 @@
         <div class="d-grid gap-2 d-md-block">
             <button class="btn btn-sm btn-primary" type="button">Unggah Skripsi</button>
         </div>
-        <hr>
-        <h4>Telusuri:</h4>
-        <p class="font-small">Anda dapat melihat arsip skripsi yang tersedia berdasarkan tahun:</p>
-        <div class="row row-cols-1 row-cols-md-5 g-4 font-smaller">
-            <a href="#" class="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
-                <div class="col">
-                    <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title text-center m-0">2025</h5>
-                    </div>
-                    </div>
-                </div>
-            </a>
-            <a href="#" class="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
-                <div class="col">
-                    <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title text-center m-0">2024</h5>
-                    </div>
-                    </div>
-                </div>
-            </a>
-            <a href="#" class="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
-                <div class="col">
-                    <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title text-center m-0">2023</h5>
-                    </div>
-                    </div>
-                </div>
-            </a>
-            <a href="#" class="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
-                <div class="col">
-                    <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title text-center m-0">2022</h5>
-                    </div>
-                    </div>
-                </div>
-            </a>
-            <a href="#" class="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
-                <div class="col">
-                    <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title text-center m-0">2021</h5>
-                    </div>
-                    </div>
-                </div>
-            </a>
-        </div>
     </div>
 @endsection

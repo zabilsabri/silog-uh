b7b487c50108f0d8ac5e593a66a151638ebf758d
zabilsabri <zabilsabrimuhammad0@gmail.com>
2025-05-30 00:19:40 +0800

    `feat`: update sidebar for admin role with dashboard and student links

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -4,9 +4,17 @@
         <p class="p-0 m-0">Halo,</p>
         <p class="p-0 m-0 fw-bolder">Selamat Datang</p>
     </div>
-    <a href="{{ route('home') }}" class="{{ request()->routeIs('home') ? 'active' : '' }} my-1"><span class="ps-3 font-small"><i class="fi fi-rs-house-chimney me-2"></i>Beranda</span></a>
+    @if(Auth::check() && Auth::user()->role == 'admin')
+        <a href="{{ route('home-admin') }}" class="{{ request()->routeIs('home-admin') ? 'active' : '' }} my-1"><span class="ps-3 font-small"><i class="fi fi-rs-house-chimney me-2"></i>Dashboard</span></a>
+        <a href="{{ route('students.admin') }}" class="{{ request()->routeIs('students.admin') || request()->routeIs('students.detail.admin') ? 'active' : '' }} my-1"><span class="ps-3 font-small"><i class="fi fi-rr-users-alt me-2"></i>Mahasiswa</span></a>
+    @else
+        <a href="{{ route('home') }}" class="{{ request()->routeIs('home') ? 'active' : '' }} my-1"><span class="ps-3 font-small"><i class="fi fi-rs-house-chimney me-2"></i>Beranda</span></a>
+    @endif
     <a href="{{ route('thesis') }}" class="{{ request()->routeIs('thesis') || request()->routeIs('thesis.detail') ? 'active' : '' }} my-1"><span class="ps-3 font-small"><i class="fi fi-rs-document me-2"></i>Skripsi</span></a>
     <hr>
-    <a href="{{ route('profile') }}" class="{{ request()->routeIs('profile') ? 'active' : '' }} my-1"><span class="ps-3 font-small"><i class="fi fi-rs-user me-2"></i>Akun</span></a>
+    @if(Auth::check() && Auth::user()->role == 'admin')
+    @else
+        <a href="{{ route('profile') }}" class="{{ request()->routeIs('profile') ? 'active' : '' }} my-1"><span class="ps-3 font-small"><i class="fi fi-rs-user me-2"></i>Akun</span></a>
+    @endif
     <a href="#" class="my-1"><span class="ps-3 font-small"><i class="fi fi-rs-book me-2"></i>Tutorial</span></a>
 </div>

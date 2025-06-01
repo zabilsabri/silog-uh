6986e78bef6cc804d508b1dd0a69f3a7e785d5b0
zabilsabri <zabilsabrimuhammad0@gmail.com>
2025-06-01 12:29:32 +0800

    `feat`: implement UsersImport class for importing users from Excel

---
+++ app/Imports/UsersImport.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\User;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Illuminate\Support\Facades\Hash;
+
+class UsersImport implements ToModel, WithHeadingRow
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        return User::updateOrCreate(
+            ['username' => $row['nim']],
+            [
+                'password' => Hash::make($row['nim']),
+            ]
+        );
+
+    }
+}
